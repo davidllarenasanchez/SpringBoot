e87f66b648349bbb156caf1b35cee20ed33e2f21
davidllarenasanchez <david.llarena.sanchez@gmail.com>
2020-04-13 21:04:46 +0200

    [ SPB1 ] First Application to be uploaded

---
+++ HelloWorldSpringBoot/src/main/java/com/helloworldspringboot/HelloWorldSpringBootApplication.java
@@ -0,0 +1,24 @@
+package com.helloworldspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s! from First Boot example", name);
+        //http://localhost:8080/hello
+    }
+
+}
