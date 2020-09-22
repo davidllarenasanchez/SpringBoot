584cf2feffd4e7ef26d0f0514c4fafc1de7b2cb7
davidllarenasanchez <david.llarena.sanchez@gmail.com>
2020-09-22 20:43:37 +0200

    [ Second Commit] Added Transactional to the project.

--- FirstProjectSpringBootH2/src/main/java/com/springboot/h2/restservicio/ServicioRestCoches.java
+++ FirstProjectSpringBootH2/src/main/java/com/springboot/h2/restservicio/ServicioRestCoches.java
@@ -6,8 +6,12 @@ import com.springboot.h2.modelo.servicio.ICochesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,11 +24,13 @@ public class ServicioRestCoches {
     ICochesService  iCochesService;
 
     @GetMapping(value="allCoches")
+    @Transactional(readOnly = true)
     public List<Coches> getAllCoches() {
         return iCochesService.getAllCoches();
     }
 
     @PostMapping(value="addCoches")
+    @Transactional
     public Coches addCoches(@RequestBody Coches coche)  {
         return iCochesService.addCoche(coche);
         /*
@@ -38,19 +44,30 @@ public class ServicioRestCoches {
     }
 
     @PutMapping(value="updateCoches")
+    @Transactional
     public Coches updateCoche(@RequestBody Coches coche)  {
         return iCochesService.updateCoche(coche);
     }
 
     @DeleteMapping(value="borrarCoches/{id}" )
+    @Transactional
     public boolean deleteCoches(@PathVariable(value="id") Long id) {
         iCochesService.removeCocheById(id);
         return true;
     }
 
     @GetMapping(value="getcoches/{id}" )
-    public Optional<Coches> getCochesById(@PathVariable(value="id") Long id) {
-        return iCochesService.getCocheById(id);
+    @Transactional(readOnly = true)
+    public Optional<Coches> getCochesById(@PathVariable(value="id") Long id) throws FileNotFoundException {
+        Optional<Coches> coche = iCochesService.getCocheById(id);
+        File file = new File(System.getProperty("user.dir")+"\\test1.txt");
+        file.setWritable(true);
+         PrintWriter pw = new PrintWriter(file);
+         pw.println(coche);
+         pw.close();
+         System.out.println(System.getProperty("user.dir"));
+
+        return coche;
 
     }
 }
