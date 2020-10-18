fde7b44b7edeb8b7069434865713730d7f42ce59
davidllarenasanchez <david.llarena.sanchez@gmail.com>
2020-10-18 18:39:05 +0200

    [FourthCommit] Calculadora Controller Integration Test: added test method for controller method suma.

--- Calculadora/src/test/java/com/calculadora/controllertest/CalculadoraControllerIntegrationTest.java
+++ Calculadora/src/test/java/com/calculadora/controllertest/CalculadoraControllerIntegrationTest.java
@@ -1,13 +1,45 @@
 package com.calculadora.controllertest;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class CalculadoraControllerIntegrationTest {
+
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void suma() {
+
+        ResponseEntity<Double> response = this.restTemplate.getForEntity("http://localhost:" + port + "/calculadora/suma?operador1=2&operador2=2",  Double.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat(response.getBody(), equalTo(4.0));
+
+    }
+
+
+
+
+
 }
